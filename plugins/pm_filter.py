ff74fa3913342196a6bb6760d818c8a51ce14d85
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-11 16:58:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("❗️It will not work for U, as it was not Requested by U. 😐 U can Ask What u want 🖍", show_alert=True)
     try:
         offset = int(offset)
     except:
