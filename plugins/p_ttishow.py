e984ab3343b6c647b910d09b4ae700e21ec86444
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-11 17:36:00 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -45,7 +45,7 @@ async def save_group(bot, message):
             text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
             reply_markup=reply_markup)
     else:
-        for u in message.new_chat_members:
+        for u in message.neew_chat_members:
             if (temp.MELCOW).get('welcome') is not None:
                 try:
                     await (temp.MELCOW['welcome']).delete()
