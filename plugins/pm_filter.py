d2b6aea281fbfce7a20249e5e775fe3de9116369
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-21 02:11:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -675,7 +675,7 @@ async def auto_filter(client, msg, spoll=False):
             url = imdb['url']
         )
     else:
-        cap = f"{message.from_user.first_name}, Here is what i found for your query {search}"
+        cap = f"{message.from_user.mention}, Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
