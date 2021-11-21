23ce0f8778cbac894f5fdc7a32a107a3fe127ed1
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-22 00:49:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/search_dentalbooks_bot> 📚𝙳𝚎𝚗𝚝 𝚃𝚎𝚌𝚑 🔎 𝙻𝚒𝚋𝚛𝚊𝚛𝚒𝚊𝚗🧚‍♀</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚈𝙾𝚄 𝙰𝚅𝙰𝙸𝙻𝙰𝙱𝙻𝙴 𝙳𝙴𝙽𝚃𝙰𝙻 𝚁𝙴𝙵𝙴𝚁𝙴𝙽𝙲𝙴𝚂 𝙵𝚁𝙴𝙴, 𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 <a href='https://t.me/dent_tech_for_books'> 📚 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🔎 𝙶𝚁𝙾𝚄𝙿 🧚‍♀ ♀</a> 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/search_dentalbooks_bot> 📚𝙳𝚎𝚗𝚝 𝚃𝚎𝚌𝚑 🔎 𝙻𝚒𝚋𝚛𝚊𝚛𝚒𝚊𝚗🧚‍♀</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚈𝙾𝚄 𝙰𝚅𝙰𝙸𝙻𝙰𝙱𝙻𝙴 𝙳𝙴𝙽𝚃𝙰𝙻 𝚁𝙴𝙵𝙴𝚁𝙴𝙽𝙲𝙴𝚂 𝙵𝚁𝙴𝙴, 𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 <a href='https://t.me/dent_tech_for_books'> 📚 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🔎 𝙶𝚁𝙾𝚄𝙿 🧚‍♀ </a> 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
