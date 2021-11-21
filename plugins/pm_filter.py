a988ee7b80c30384ff373d68aedbc2a05dc25d14
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-21 09:51:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -427,14 +427,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption
-            reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton
-                            (
-                                'Developers', url="https://t.me/CrazyBotsz"
-                            )
-                    ]
+                    InlineKeyboardButton
+                        (
+                            'Developers', url="https://t.me/CrazyBotsz"
+                        )
+                    
                 ]
             )       
         )           
