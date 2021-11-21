350768d45954fd54c03895294509797541ebc3d3
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-21 08:53:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -431,7 +431,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption
-            parse_mode="html"
+            
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
