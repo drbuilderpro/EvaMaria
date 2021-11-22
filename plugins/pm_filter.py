3a24f43d1dbe7efca8251daa4512eadbb7c337bd
drbuilderpro <74030947+drbuilderpro@users.noreply.github.com>
2021-11-22 13:34:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,9 +61,9 @@ async def give_filter(client,message):
                         buttons= [[
                              InlineKeyboardButton('🤖 Updates', url='https://t.me/dent_tech_for_books')
                              ],[
-                             InlineKeyboardButton('🤖 Updates', url='https://t.me/dent_tech_for_books'),
+                             InlineKeyboardButton('🤖 Updates', url='https://t.me/dent_tech_for_books')
                          ]]
-                        button = eval(btn) 
+                       
                         await message.reply_cached_media(
                             fileid,
                             caption=reply_text or "",
